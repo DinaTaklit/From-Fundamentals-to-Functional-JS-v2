ca4cd5fdd3c07472dfc2b3008a808b779b9740f4
DinaTaklit <ez_taklit@esi.dz>
2020-10-23 09:12:55 +0100

    Objects: Game chars implementation

---
+++ GameCharacters.js
@@ -0,0 +1,9 @@
+// Create an object using breacket and dot notation that represents the characters and related data you may find in a game of clue.
+
+var game = {}; // Initial that as object literal
+game.murder = '??';
+game['weapons'] = ['lasers', 'angry cats', 'dish soap']; // since it is a list of weapons only we can use arrays.
+game.name = []; // array literal
+game.name = [];
+game.name[0] = 'Miss Scarlet';
+game.name.push('Mr. Green');
