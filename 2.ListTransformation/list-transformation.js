b97b74dd1c3fb6959a0f085e8b291f3d778ce3d2
DinaTaklit <ez_taklit@esi.dz>
2020-10-24 14:30:45 +0100

    2.List Transformation: Loop though all the properties of an object

--- 2.ListTransformation/list-transformation.js
+++ 2.ListTransformation/list-transformation.js
@@ -11,10 +11,27 @@ const game = {
     ]
 }
 
-// Loop through the suspects array of objects using foo
+// Loop through the suspects array 
 function foo (){
     for (let i = 0; i < game.suspects.length; i++){
         console.log(game.suspects[i]);
     }
 }
 foo();
+
+// Loop though all the properties of the suspect objects in the suspects array. 
+// make them if you think they are guilty
+let gameLoop = function(game){
+    for (let i = 0; i< game.suspects.length; i++){
+        console.log('outerloop');
+        for (let key in game.suspects[i]){
+            console.log('inner loop');
+            if (game.suspects[i][key] === 'Rusty'){
+                console.log('Found \'em !');
+            }else {
+                console.log('next time!');
+            }
+        }
+    }
+}
+gameLoop(game);
